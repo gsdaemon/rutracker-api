5e21756a9daa1777f91e88fdeebda616987bfe12
Daemon <gsdaemon@gmail.com>
2019-11-14 21:03:13 +0200

    Added possibility to search by categories. Fixed torrent size parsing. Added possibility to instantiate the class with a custom host, username and password.

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const Parser = require("./lib/parser");
 const PageProvider = require("./lib/page-provider");
 
 class RutrackerApi {
-  constructor() {
-    this.parser = new Parser();
-    this.pageProvider = new PageProvider();
+  constructor({ host = "http://rutracker.org", username, password }) {
+    this.authInfo = { username, password };
+    this.parser = new Parser(host);
+    this.pageProvider = new PageProvider(host);
   }
 
-  login({ username, password }) {
+  login({ username, password } = {}) {
+    if (!username || !password) {
+      ({username, password} = this.authInfo);
+    }
     return this.pageProvider.login(username, password);
   }
 
-  search({ query, sort, order }) {
+  search({ query, sort, order, cats }) {
     return this.pageProvider
-      .search({ query, sort, order })
+      .search({ query, sort, order, cats })
       .then(html => this.parser.parseSearch(html));
   }
 
